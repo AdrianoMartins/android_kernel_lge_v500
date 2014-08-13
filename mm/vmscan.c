4145acefc4e188e06f5b5fb8a7a44c7057bc3eea
dr.beat <xgecco@gmail.com>
2014-08-13 12:43:21 +0200

    Linux 3.4.96 Part 1

--- mm/vmscan.c
+++ mm/vmscan.c
@@ -2873,6 +2873,11 @@ static int kswapd(void *p)
 						&balanced_classzone_idx);
 		}
 	}
+
+	tsk->flags &= ~(PF_MEMALLOC | PF_SWAPWRITE | PF_KSWAPD);
+	current->reclaim_state = NULL;
+	lockdep_clear_current_reclaim_state();
+
 	return 0;
 }
 
