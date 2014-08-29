d87da7446d3bf5f46ee7dc01bc6074ff35ffe19c
dr.beat <xgecco@gmail.com>
2014-08-29 13:07:28 +0200

    buildscript: fancy up the buildscript

--- build.sh
+++ build.sh
@@ -1,25 +1,52 @@
+#!/bin/bash
+
+# Add Colors to unhappiness
+green='\033[01;32m'
+red='\033[01;31m'
+restore='\033[0m'
+
+clear
+
+# Kernel Version
+BASE_KVER="mani"
+VER="_v17"
+KVER=$BASE_KVER$VER
+
+echo -e "${green}"
 echo ">>> set prerequisites"
+echo -e "${restore}"
+export LOCALVERSION="~"`echo $KVER`
 export ARCH=arm
 export SUBARCH=arm
 export CROSS_COMPILE=/home/mani/android/a15-linaro-4.9.1/bin/arm-cortex_a15-linux-gnueabihf-
 #make mani_v500_defconfig
 #make menuconfig
 
+echo -e "${green}"
 echo ">>> build zImage"
+echo -e "${restore}"
 make -j3
 
+echo -e "${green}"
 echo ">>> copy zImage to boot/kernel/<"
+echo -e "${restore}"
 cp arch/arm/boot/zImage ~/android/boot_aosp/kernel/
 
+echo -e "${green}"
 echo ">>> copy modules to boot/system/lib/modules<"
+echo -e "${restore}"
 find ~/android/v500_aosp -name "*.ko" -exec cp {} ~/android/boot_aosp/system/lib/modules/ \;
 
-zipfile="mani_aosp.zip"
+zipfile=$KVER
+echo -e "${green}"
 echo ">>> build zipfile"
+echo -e "${restore}"
 cd ~/android/boot_aosp/
 rm -f *.zip
 zip -9 -r $zipfile *
 rm -f /tmp/*.zip
 cp *.zip /tmp
 
+echo -e "${red}"
 echo ">>> ZIP is ready"
+echo -e "${restore}"
