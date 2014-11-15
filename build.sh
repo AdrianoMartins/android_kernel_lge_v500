a08c225bb3f0cfa1200322c85a389518522b4002
Adriano Martins <adrianomartins@gmail.com>
2014-11-15 19:34:17 -0200

    Build script and defconfig file

--- build.sh
+++ build.sh
@@ -7,41 +7,51 @@ restore='\033[0m'
 
 clear
 
-# Kernel Version
-BASE_KVER="mani"
-VER="_v18"
-KVER=$BASE_KVER$VER
+# Folders
+BASE_DIR="$HOME/Android/kernel"
 
+# Kernel Version
+BASE_KVER="aosp_lollipop"
+VER="_v1.0"
+KVER=$BASE_KVER$VER
+DEFCONF="aosp_v500_defconfig"
+
+# Directories & jobs
+JOBS=`grep -c "processor" /proc/cpuinfo`
+KERNEL_DIR="$BASE_DIR/v500_kernel_aosp"
+ZIP_DIR="$BASE_DIR/boot_v500_kernel_aosp"
+
+# start building
 echo -e "${green}"
 echo ">>> set prerequisites"
 echo -e "${restore}"
-export LOCALVERSION="~"`echo $KVER`
+export LOCALVERSION="-"`echo $KVER`
 export ARCH=arm
 export SUBARCH=arm
-export CROSS_COMPILE=/home/mani/android/a15-linaro-4.9.2/bin/arm-cortex_a15-linux-gnueabihf-
-#make mani_v500_defconfig
-#make menuconfig
+export CROSS_COMPILE="$BASE_DIR/toolchain/arm-cortex_a15-linux-gnueabihf-linaro_4.9.2-2014.10/bin/arm-cortex_a15-linux-gnueabihf-"
 
 echo -e "${green}"
 echo ">>> build zImage"
 echo -e "${restore}"
-make -j3
+make $DEFCONF
+make -j$JOBS
 
 echo -e "${green}"
-echo ">>> copy zImage to boot/kernel/<"
+echo ">>> copy zImage"
 echo -e "${restore}"
-cp arch/arm/boot/zImage ~/android/boot_aosp/kernel/
+cp arch/arm/boot/zImage $ZIP_DIR/kernel/
 
 echo -e "${green}"
-echo ">>> copy modules to boot/system/lib/modules<"
+echo ">>> copy modules"
 echo -e "${restore}"
-find ~/android/v500_aosp -name "*.ko" -exec cp {} ~/android/boot_aosp/system/lib/modules/ \;
+rm $ZIP_DIR/system/lib/modules/*
+find $KERNEL_DIR -name "*.ko" -exec cp {} $ZIP_DIR/system/lib/modules/ \;
 
 zipfile=$KVER
 echo -e "${green}"
 echo ">>> build zipfile"
 echo -e "${restore}"
-cd ~/android/boot_aosp/
+cd $ZIP_DIR
 rm -f *.zip
 zip -9 -r $zipfile *
 rm -f /tmp/*.zip
